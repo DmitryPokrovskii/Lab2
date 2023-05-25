38279772701731cf855b383cf2536e0426cd401d
Dmitry Pokrovskii <125653789+DmitryPokrovskii@users.noreply.github.com>
2023-05-26 00:18:22 +0300

    RowScreen.kt 2

--- app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreen.kt
@@ -2,8 +2,16 @@ package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.Screen
 
 val THREE_ELEMENTS_LIST = listOf(R.string.first, R.string.second, R.string.third)
@@ -21,5 +29,17 @@ fun RowScreen(){
 
 @Composable
 fun MyRow() {
-    TODO("Not yet implemented")
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()){
+
+        THREE_ELEMENTS_LIST.forEach{ textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 18.sp
+            )
+        }
+
+
+    }
 }
