e5b98b6d53314f8b5a38cb7c6a8e25d124f167ad
Dmitry Pokrovskii <125653789+DmitryPokrovskii@users.noreply.github.com>
2023-05-26 00:30:12 +0300

    BoxScreen.kt,ColumnScreen,NavigationScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ColumnScreen.kt
@@ -3,7 +3,15 @@ package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.BackButtonHandler
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.Screen
 
 @Composable
@@ -17,5 +25,19 @@ fun ColumnScreen(){
 
 @Composable
 fun MyColumn() {
-    TODO("soon")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ){
+
+        THREE_ELEMENTS_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 30.sp
+            )
+        }
+
+
+    }
 }
